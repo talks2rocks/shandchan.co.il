67c44b856107210efa24d3cc811903290680f034
Talks2Rocks <perlman.yoel@gmail.com>
2013-04-09 16:56:51 +0300

    Add initial test for help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,4 +9,12 @@ describe "Static pages" do
       page.should have_content('Sample App')
     end
   end
+
+  describe "help page" do
+
+    it "should have the content 'Help Page'" do 
+      visit 'static_pages/help'
+      page.should have_content("Help")
+    end
+  end
 end
